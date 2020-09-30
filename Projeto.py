0371acbfe283044c2b3075c09f7250ae7c1a628a
Luiz fernando Reichert <nandoreichert1506@gmail.com>
2020-09-30 17:27:29 -0300

    Projeto_Publica_Proway

--- Projeto.py
+++ Projeto.py
@@ -17,40 +17,60 @@ def chama_consulta():
     comando_SQL = 'select num_cad_jogo,placar_cad_jogo,min_temporada,max_temporada,quebra_recorde_min,quebra_recorde_max from cadastro_jogo'
     cursor.execute(comando_SQL)
     dados_lidos = cursor.fetchall()
-    print(dados_lidos)
     
-
     consulta.tableWidget.setRowCount(len(dados_lidos))
     consulta.tableWidget.setColumnCount(6)
-
+    
     for i in range(0, len(dados_lidos)):
-        for j in range(0,6):
+       for j in range(0,6):
             consulta.tableWidget.setItem(i,j,QtWidgets.QTableWidgetItem(str(dados_lidos[i][j])))
     
-
 def funcao_principal():
     
+    cursor = banco.cursor()
+    SQL = 'select min(min_temporada) from cadastro_jogo' 
+    cursor.execute(SQL,)
+    dados_min = cursor.fetchall()
+
+    cursor = banco.cursor()
+    SQL_ = 'select max(max_temporada) from cadastro_jogo' 
+    cursor.execute(SQL_)
+    dados_max = cursor.fetchall()
+
+    cursor = banco.cursor()
+    SQL_ = 'select max(quebra_recorde_min) from cadastro_jogo' 
+    cursor.execute(SQL_)
+    dados_quebra_rcd_min = cursor.fetchall()
+
+    cursor = banco.cursor()
+    SQL_ = 'select max(quebra_recorde_max) from cadastro_jogo' 
+    cursor.execute(SQL_)
+    dados_quebra_rcd_max = cursor.fetchall()
+    
+    min_temp = dados_min[0][0]
+    max_temp = dados_max[0][0]
     qbr_rcd_min = 0
     qbr_rcd_max = 0
-    min_temp =  'select max(min_temporada) from cadastro_jogo'
-    max_temp =  'select max(max_temporada) from cadastro_jogo'
     
     campo1 = cadastro.lineEdit.text()
     campo2 = int(cadastro.lineEdit_2.text())
     cadastro.lineEdit.setText('')
     cadastro.lineEdit_2.setText('')
-    
-
-    if campo2 > max_temp:
+    if (max_temp == None) and (min_temp == None):
         max_temp = campo2
-        min_temp = min_temp
-        qbr_rcd_max = 1
+        min_temp = campo2
+        qbr_rcd_max = 0
         qbr_rcd_min= 0
+    elif campo2 >= max_temp:
+        max_temp = campo2
+        min_temp = dados_min[0][0]
+        qbr_rcd_max = 1
+        qbr_rcd_min= dados_quebra_rcd_min[0][0]
     else:
         min_temp = campo2
-        max_temp = max_temp
+        max_temp = dados_max[0][0]
         qbr_rcd_min = 1
-        qbr_rcd_max = 0
+        qbr_rcd_max = dados_quebra_rcd_max[0][0]
 
 
     cursor = banco.cursor()
@@ -58,6 +78,7 @@ def funcao_principal():
     dados = int(campo1),int(campo2),int(min_temp),int(max_temp),int(qbr_rcd_min),int(qbr_rcd_max)
     cursor.execute(comando_SQL,dados)
     banco.commit()
+
 
 
 
--- baseDados.sql
+++ baseDados.sql
@@ -2,6 +2,7 @@ create database projeto_publica;
 
 create table cadastro_jogo(
     id_cad_jogo int not null primary key auto_increment,
+    num_cad_jogo int not null,
     placar_cad_jogo int not null,
     min_temporada int not null,
     max_temporada int not null,
