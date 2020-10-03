2a6a98eb0a4fb514b0cbdc63ef4d6e7c4fff6172
Luiz fernando Reichert <nandoreichert1506@gmail.com>
2020-10-03 10:52:50 -0300

    Projeto_Publica_Proway

--- Projeto.py
+++ Projeto.py
@@ -1,4 +1,5 @@
 from PyQt5 import uic,QtWidgets
+from PyQt5.QtWidgets import QMessageBox
 import mysql.connector
 
 banco = mysql.connector.connect(
@@ -7,9 +8,12 @@ banco = mysql.connector.connect(
     passwd= '',
     database= 'projeto_publica'
 )
+def chama_tela1():
+    tela1.show()
 
 def chama_segunda_tela():
-    cadastro.show()
+    cadastro.show()    
+
 def chama_consulta():
     consulta.show()
 
@@ -26,7 +30,6 @@ def chama_consulta():
             consulta.tableWidget.setItem(i,j,QtWidgets.QTableWidgetItem(str(dados_lidos[i][j])))
     
 def funcao_principal():
-    
     cursor = banco.cursor()
     SQL = 'select min(min_temporada) from cadastro_jogo' 
     cursor.execute(SQL,)
@@ -51,35 +54,37 @@ def funcao_principal():
     max_temp = dados_max[0][0]
     qbr_rcd_min = 0
     qbr_rcd_max = 0
-    
-    campo1 = cadastro.lineEdit.text()
-    campo2 = int(cadastro.lineEdit_2.text())
-    cadastro.lineEdit.setText('')
-    cadastro.lineEdit_2.setText('')
-    if (max_temp == None) and (min_temp == None):
-        max_temp = campo2
-        min_temp = campo2
-        qbr_rcd_max = 0
-        qbr_rcd_min= 0
-    elif campo2 >= max_temp:
-        max_temp = campo2
-        min_temp = dados_min[0][0]
-        qbr_rcd_max = 1
-        qbr_rcd_min= dados_quebra_rcd_min[0][0]
-    else:
-        min_temp = campo2
-        max_temp = dados_max[0][0]
-        qbr_rcd_min = 1
-        qbr_rcd_max = dados_quebra_rcd_max[0][0]
 
+    dado_lido = cadastro.lineEdit.text()
+    dado_lido_2 = cadastro.lineEdit_2.text()
+    if(dado_lido == '') or (dado_lido_2 == ''):
+        QMessageBox.about(cadastro,'Atenção','Dados incorretos')
+    else:
+        campo1 = cadastro.lineEdit.text()
+        campo2 = int(cadastro.lineEdit_2.text())
+        cadastro.lineEdit.setText('')
+        cadastro.lineEdit_2.setText('')
+        if (max_temp == None) and (min_temp == None):
+            max_temp = campo2
+            min_temp = campo2
+            qbr_rcd_max = 0
+            qbr_rcd_min= 0
+        elif campo2 >= max_temp:
+            max_temp = campo2
+            min_temp = dados_min[0][0]
+            qbr_rcd_max = 1
+            qbr_rcd_min= dados_quebra_rcd_min[0][0]
+        else:
+            min_temp = campo2
+            max_temp = dados_max[0][0]
+            qbr_rcd_min = 1
+            qbr_rcd_max = dados_quebra_rcd_max[0][0]
 
     cursor = banco.cursor()
     comando_SQL = 'INSERT INTO cadastro_jogo (num_cad_jogo,placar_cad_jogo,min_temporada,max_temporada,quebra_recorde_min,quebra_recorde_max) values (%s,%s,%s,%s,%s,%s)'
     dados = int(campo1),int(campo2),int(min_temp),int(max_temp),int(qbr_rcd_min),int(qbr_rcd_max)
     cursor.execute(comando_SQL,dados)
     banco.commit()
-
-
 
 
 app=QtWidgets.QApplication([])
@@ -89,6 +94,6 @@ consulta=uic.loadUi('consulta.ui')
 tela1.pushButton.clicked.connect(chama_segunda_tela)
 tela1.pushButton_2.clicked.connect(chama_consulta)
 cadastro.pushButton_2.clicked.connect(funcao_principal)
-
+cadastro.pushButton_2.clicked.connect(chama_tela1)
 tela1.show()
 app.exec()
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # ProjetoPublica
-Projeto Publica-Proway
+
+# Para executar o programa:
+
+Para fazer a execução do programa, abra o GitBash digite git clone e cole este arquivo https://github.com/LuizFernandoR/ProjetoPublica.git e precione ENTER. Abra a pasta que foi clonada, dentro da pasta abra o terminal e digite Python Projeto.py que ele vai executar.
+
+Se caso precisar instalar o Python este é o site https://www.python.org/downloads/.
+O link abaixo contém algumas versões do PyQt5 compatíveis com algumas versões do Python: 
+https://www.lfd.uci.edu/~gohlke/pythonlibs/#pyqt5
+
+
+
+
+
+
